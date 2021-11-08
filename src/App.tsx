3160bf3478d003267b6789bbab2467266376a3c3
Lambert Schulze <schulze.lambert@googlemail.com>
2021-11-08 23:59:02 +0100

    added support for redux dev tools

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,15 @@ import {
 } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import compose from 'redux-devtools-extension';
 
 import reducer from './store/reducer'
 
 import MainNavbar from './components/Navbar';
 import Game from './views/Game';
 
-const store = createStore(reducer, applyMiddleware(thunk))
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 store.subscribe(() => console.log(store.getState()))
 
